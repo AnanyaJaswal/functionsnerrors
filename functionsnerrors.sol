b75461f92a72880a5de049541ca1133b8c38bfa2
AnanyaJaswal <ananyajaswal912@gmail.com>
2024-07-04 00:25:06 +0530

    Update functionsnerrors.sol

--- functionsnerrors.sol
+++ functionsnerrors.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract DivideByZero {
-    uint public result;
-
-    function Require(uint numerator, uint denominator) public {
-        require( denominator != 0, "Denominator cannot be zero");
-        result = numerator / denominator;
+contract KpopAlbums {
+    uint public totalAlbums;
+    uint constant max_albums = 250; 
+  
+    function addAlbums(uint albums) public {
+        require(albums > 0, "NO NO NO ! The number of albums must be greater than zero.");
+        totalAlbums += albums;
+    }
+  
+    function checkAlbums() public view {
+        assert(totalAlbums <= max_albums);
     }
 
-    function Assert(uint numerator, uint denominator) public {
-        assert( denominator != 0);
-        result = numerator / denominator;
-    }
-
-    function Revert(uint numerator, uint denominator) public pure returns (uint) {
-        if( denominator == 0){
-
-        revert("Denominator cannot be zero , please provide a valid denominator which must be a positive number");
+    function removeAlbums(uint albums) public {
+        if (albums > totalAlbums) {
+            revert("Oops! You can't remove more albums than currently exist.");
         }
-        return numerator / denominator;
+        totalAlbums -= albums;
     }
 }
